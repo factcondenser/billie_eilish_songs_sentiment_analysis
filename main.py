47e6614e5b6a2efea4dfe376ceee8631bb2d72e3
factcondenser <markpan@live.com>
2020-08-31 12:58:37 -0500

    Use json instead and make plots

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-# def install(package):
-#   import pip
-#   pip.main(["install", package])
+import json, re, requests
+import plotly.graph_objects as go
 
-# install("google-cloud")
-# install("google-cloud-language")
-
-import csv, re, requests
 from bs4 import BeautifulSoup
 from google.cloud import language_v1
 from google.cloud.language_v1 import enums
 
-#######################################
-## Get all Billie Eilish song lyrics ##
-#######################################
+###############
+## CONSTANTS ##
+###############
 
-# This function builds Genius urls from Billie Eilish song titles.
-def build_genius_url(song_title):
+PATH_TO_GOOGLE_SERVICE_ACCOUNT_JSON = "./service_account_viewer.json"
+
+##########################
+## FUNCTION DEFINITIONS ##
+##########################
+
+# Uses Google Cloud's Natural Language API to provide sentiment analysis for some text.
+def analyze_sentiment(text_content):
+  client = language_v1.LanguageServiceClient.from_service_account_json(
+    PATH_TO_GOOGLE_SERVICE_ACCOUNT_JSON
+  )
+  type_ = enums.Document.Type.PLAIN_TEXT
+  language = "en"
+  document = {"content": text_content, "type": type_, "language": language}
+
+  encoding_type = enums.EncodingType.UTF8
+  return client.analyze_sentiment(document, encoding_type=encoding_type)
+
+# Builds a Genius url from a Billie Eilish song title.
+def build_genius_url_for_billie_eilish_song(song_title):
   kebab_cased = song_title. \
     replace(" ", "-"). \
     replace("!", ""). \
@@ -24,85 +37,93 @@ def build_genius_url(song_title):
     lower()
   return f"https://genius.com/Billie-eilish-{kebab_cased}-lyrics"
 
-song_titles = []
-songs = {}
+# Removes text in square brackets ("[" and "]") and adds periods to the ends of lines.
+def clean_up_genius_lyrics(lyrics):
+  return re.sub(r"([^\s.])\n", r"\1.\n", re.sub(r"\[.*\]\n", "", lyrics))
 
-page = requests.get("https://en.wikipedia.org/wiki/Category:Billie_Eilish_songs")
-soup = BeautifulSoup(page.content, "html.parser")
+# Fetches items from a Wikipedia set category page.
+def fetch_items_from_wikipedia_category(category_name):
+  items = []
 
-groups = soup.find_all(class_="mw-category-group")
-for group in groups:
-  links = group.find_all("a")
-  for link in links:
-    tidied = re.sub(r"\(.*\)", "", link.get_text()).strip()
-    song_titles.append(tidied)
+  page = requests.get(f"https://en.wikipedia.org/wiki/Category:{category_name}")
+  soup = BeautifulSoup(page.content, "html.parser")
 
-for song_title in song_titles:
-  page = requests.get(build_genius_url(song_title))
-  if page.status_code == 200:
-    print(f"Fetched \"{song_title}\"")
-  else:
-    print(f"Failed to fetch \"{song_title}\"")
-    continue
+  groups = soup.find_all(class_="mw-category-group")
+  for group in groups:
+    links = group.find_all("a")
+    for link in links:
+      tidied = re.sub(r"\(.*\)", "", link.get_text()).strip()
+      items.append(tidied)
+
+  return items
+
+# Fetches song lyrics from a Genius url.
+def fetch_genius_lyrics(genius_url):
+  page = requests.get(genius_url)
+  if not page.ok:
+    print(f"Failed to fetch from \"{genius_url}\": Status {page.status_code}")
+    return
 
   soup = BeautifulSoup(page.content, "html.parser")
 
   lyrics_div = soup.find(class_="lyrics")
-  lyrics = lyrics_div.find("p").get_text()
-  songs[song_title] = lyrics
+  return lyrics_div.find("p").get_text()
 
-  print(f"Stored lyrics for \"{song_title}\"\n")
+#################
+## MAIN SCRIPT ##
+#################
 
-##################################################################
-## Perform sentiment analysis on all Billie Eilish song lyrics  ##
-## using Google Cloud's Natural Language API (requires account) ##
-##################################################################
+songs = []
 
-PATH_TO_ACCOUNT_JSON = "./service_account_viewer.json"
+song_titles = fetch_items_from_wikipedia_category("Billie_Eilish_songs")
+for song_title in song_titles:
+  genius_url = build_genius_url_for_billie_eilish_song(song_title)
+  lyrics = fetch_genius_lyrics(genius_url)
+  tidy_lyrics = clean_up_genius_lyrics(lyrics)
+  analysis_result = analyze_sentiment(tidy_lyrics)
 
-def analyze_sentiment(text_content):
-  client = \
-    language_v1.LanguageServiceClient.from_service_account_json(PATH_TO_ACCOUNT_JSON)
-  type_ = enums.Document.Type.PLAIN_TEXT
-  language = "en"
-  document = {"content": text_content, "type": type_, "language": language}
+  song = {
+    "title": song_title,
+    "score": analysis_result.document_sentiment.score,
+    "magnitude": analysis_result.document_sentiment.magnitude,
+    "lines": []
+  }
 
-  encoding_type = enums.EncodingType.UTF8
-  return client.analyze_sentiment(document, encoding_type=encoding_type)
-
-analyzed_songs = {}
-
-for (title, lyrics) in songs.items():
-  tidy_lyrics = re.sub(r"(\w)\n", r"\1.\n", re.sub(r"\[.*\]\n", "", lyrics))
-  response = analyze_sentiment(tidy_lyrics)
-
-  analyzed_song = [{
-    "line": title,
-    "score": response.document_sentiment.score,
-    "magnitude": response.document_sentiment.magnitude
-  }]
-
-  for s in response.sentences:
-    analyzed_song.append({
-      "line": s.text.content,
-      "score": s.sentiment.score,
-      "magnitude": s.sentiment.magnitude
+  for sentence in analysis_result.sentences:
+    song["lines"].append({
+      "text": sentence.text.content,
+      "score": sentence.sentiment.score,
+      "magnitude": sentence.sentiment.magnitude
     })
 
-  analyzed_songs[title] = analyzed_song
+  songs.append(song)
 
-#####################################
-## Write sentiment scores to a CSV ##
-#####################################
+# Write songs data to a JSON file.
+with open("analyzed_songs.json", "w") as json_file:
+  json.dump(songs, json_file)
 
-with open("./billie_elish_songs_sentiment_scores.csv", mode="w") as csv_file:
-  fieldnames = ["line", "score", "magnitude"]
-  writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+# Visualize the data with plotly.
+with open("./analyzed_songs.json") as json_file:
+  songs = json.load(json_file)
 
-  writer.writeheader()
+for song in songs:
+  title, overall_score, overall_magnitude, lines = song.values()
+  texts, scores, magnitudes = [], [], []
+  for i, line in enumerate(lines):
+    text, score, magnitude = line.values()
+    texts.append(f"L{i + 1}: {text}")
+    scores.append(score)
+    magnitudes.append(magnitude)
 
-  for line_dicts in analyzed_songs.values():
-    for line_dict in line_dicts:
-      writer.writerow(line_dict)
+  fig = go.Figure(
+    data=[go.Bar(x=texts, y=scores)],
+    layout=go.Layout(
+        title=go.layout.Title(
+          text=f"{title} (overall score: {round(overall_score, 2)})",
+          font=dict(size=24)
+        ),
+        yaxis=dict(title="sentiment score")
+    )
+  )
 
-    writer.writerow({})
+  fig.write_html(f"./charts/{title}.html")
